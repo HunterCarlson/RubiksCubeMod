2046f21d310ed471d0d9d0f5022c9761e457b10a
Hunter Carlson <carlson.hunter@gmail.com>
2020-04-22 13:06:17 -0700

    init

---
+++ src/main/java/dev/zorink/rubikscubemod/RubiksCubeMod.java
@@ -0,0 +1,31 @@
+package dev.zorink.rubikscubemod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = RubiksCubeMod.MODID, name = RubiksCubeMod.NAME, version = RubiksCubeMod.VERSION)
+public class RubiksCubeMod
+{
+    public static final String MODID = "rubikscubemod";
+    public static final String NAME = "Rubik's Cube Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
